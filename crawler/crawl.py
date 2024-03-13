14e8d5b58317ace464cf931cb7f1226d2744869c
Xardas97 <markomicovic19@yahoo.com>
2024-03-13 16:11:45 +0100

    Crawler | Parse relevant data from voting records

--- crawler/crawl.py
+++ crawler/crawl.py
@@ -1,3 +1,4 @@
+import re
 import os.path
 import requests
 from bs4 import BeautifulSoup
@@ -8,8 +9,8 @@ def crawl_all():
    subjects_file = open(SUBJECTS_FILE_PATH)
    subjects = subjects_file.read().splitlines()
 
-   subject = subjects[0]
-   crawl(ParamBody.GENERAL_ASSEMBLY, ParamVote.YES, subject, 2015)
+   subject = subjects[6]
+   crawl(ParamBody.SECURITY_COUNCIL, ParamVote.YES, subject, 1999)
 
 def crawl(body, vote, subject, date):
    url = build_url(body, vote, subject, date)
@@ -18,7 +19,7 @@ def crawl(body, vote, subject, date):
    source_code = requests.get(url, verify=get_certificate())
 
    soup_object = BeautifulSoup(source_code.text, 'html.parser')
-   for link in soup_object.find_all('a', {'class': 'moreinfo'}, True, "Detailed record"):
+   for link in soup_object.find_all('a', {'class': 'moreinfo'}, string="Detailed record"):
       record = link.get('href')
       print(record)
       crawl_record(record)
@@ -29,10 +30,18 @@ def crawl_record(link):
    source_code = requests.get(url, verify=get_certificate())
 
    soup_object = BeautifulSoup(source_code.text, 'html.parser')
-   values = soup_object.find_all('span', {'class': 'value'})
 
-   title = values[0].string
-   print(title)
+   title = soup_object.find('span', {'class': 'title'}, string=re.compile('^Title\\s*$')).nextSibling.string
+   date = soup_object.find('span', {'class': 'title'}, string=re.compile('^Vote date\\s*$')).nextSibling.contents[0].string
+   resolution = soup_object.find('span', {'class': 'title'}, string=re.compile('^Resolution\\s*$')).nextSibling.contents[0].string
+
+   voting_data_tag = soup_object.find('span', {'class': 'title'}, string=re.compile('^Vote\\s*$')).nextSibling
+   voting_data = to_text_with_br_tags_replaced(voting_data_tag)
+
+   print("Title: " + title)
+   print("Resolution: " + resolution)
+   print("Date: " + date)
+   print("Voting data:\n" + voting_data)
 
 def get_certificate():
    if os.path.isfile(CERTIFICATE_PATH):
@@ -59,5 +68,16 @@ def build_url(body, vote, subject, date):
 
 def add_param(url, param_tag, param_value):
     return url + "&" + param_tag + "=" + param_value
+
+def to_text_with_br_tags_replaced(tag):
+   text = ''
+
+   for child in tag.recursiveChildGenerator():
+      if child.name == 'br':
+         text += '\n'
+      elif isinstance(child, str):
+         text += child.strip()
+
+   return text
 
 crawl_all()
