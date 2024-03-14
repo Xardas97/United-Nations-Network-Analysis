8a064fe8f015828517847429538433a36309f495
Xardas97 <markomicovic19@yahoo.com>
2024-03-14 15:02:05 +0100

    Crawler | Allow custom number of records per page and improve printing

--- crawler/constants.py
+++ crawler/constants.py
@@ -7,12 +7,15 @@ BASE_URL = "https://digitallibrary.un.org"
 BASE_SEARCH_URL = BASE_URL + "/search?cc=Voting Data"
 RECORD_URL = BASE_URL + "/record/{}"
 
+RECORDS_PER_PAGE = 20
+
 class ParamTag(str, Enum):
    BODY = "fct__2"
    VOTE = "fct__9"
    SUBJECT = "fct__8"
    DATE = "fct__3"
    FIRST_RECORD = "jrec"
+   PER_PAGE = "rg"
 
 class ParamBody(str, Enum):
    GENERAL_ASSEMBLY = "General Assembly"
--- crawler/crawl.py
+++ crawler/crawl.py
@@ -25,7 +25,8 @@ def crawl_all():
    subjects_file = open(SUBJECTS_FILE_PATH)
    subjects = subjects_file.read().splitlines()
 
-   record_count = crawl(ParamBody.SECURITY_COUNCIL, ParamVote.YES, subjects[6], 1999)
+   record_count = 0
+   record_count += crawl(ParamBody.SECURITY_COUNCIL, ParamVote.YES, subjects[6], 1999)
    record_count += crawl(ParamBody.SECURITY_COUNCIL, ParamVote.YES, subjects[27], 1999)
 
    print("Total crawled: " + str(record_count))
@@ -33,24 +34,30 @@ def crawl_all():
       print("")
       print(record.__str__())
 
-def crawl(body, vote, subject, date, first_record = 1):
-   url = build_search_url(body, vote, subject, date, first_record)
-   print("Accessing: [Body: {}, Vote: {}, Subject: {}, Date: {}]".format(body.value, vote.value, subject, date))
+def crawl(body, vote, subject, date, page = 0):
+   if page == 0:
+      print("Accessing: [Body: {}, Vote: {}, Subject: {}, Date: {}]".format(fpvp(body), fpvp(vote), fpp(subject), fpp(date)))
+
+   url = build_search_url(body, vote, subject, date, page)
 
    source_code = requests.get(url, verify=get_certificate())
    soup_object = BeautifulSoup(source_code.text, 'html.parser')
 
    record_count = 0
    for link in soup_object.find_all('abbr', {'class': 'unapi-id'}):
-      record_count += 1
       record_id = link.get('title')
-      process_record(record_id, subject)
+      newRecord = process_record(record_id, subject)
+      if newRecord:
+         record_count += 1
 
    nextPageTag = soup_object.find('img', {'alt': 'next'})
    if nextPageTag:
-      record_count += crawl(body, vote, subject, date, first_record + 50)
+      print("Moving to page {}...".format(page + 1))
+      record_count += crawl(body, vote, subject, date, page + 1)
 
-   print("Crawled in this search: {}\n".format(record_count))
+   if page == 0:
+      print("Crawled in this search: {}\n".format(record_count))
+
    return record_count
 
 def process_record(record_id, subject):
@@ -58,9 +65,12 @@ def process_record(record_id, subject):
    if not existing_record:
       print("Creating new record {}".format(record_id))
       records[record_id] = crawl_record(record_id, subject)
+      return True
    else:
-      print("Adding subject \"{}\" to record {}".format(subject, record_id))
-      existing_record.subjects.add(subject)
+      if subject:
+         print("Adding subject \"{}\" to record {}".format(subject, record_id))
+         existing_record.subjects.add(subject)
+      return False
 
 def crawl_record(record_id, subject):
    url = build_record_url(record_id)
@@ -83,7 +93,7 @@ def get_certificate():
 
    return None
 
-def build_search_url(body, vote, subject, date, first_record):
+def build_search_url(body, vote, subject, date, page):
    url = BASE_SEARCH_URL
 
    if body:
@@ -98,6 +108,9 @@ def build_search_url(body, vote, subject, date, first_record):
    if date:
       url = add_param(url, ParamTag.DATE, str(date))
 
+   url = add_param(url, ParamTag.PER_PAGE, str(RECORDS_PER_PAGE))
+
+   first_record = page * RECORDS_PER_PAGE + 1
    url = add_param(url, ParamTag.FIRST_RECORD, str(first_record))
 
    return url
@@ -118,5 +131,13 @@ def to_text_with_br_tags_replaced(tag):
          text += child.strip()
 
    return text
+
+# friendly param print
+def fpp(val):
+   return val if val else "All"
+
+# friendly param value print
+def fpvp(val):
+   return val.value if val and val.value else "All"
 
 crawl_all()
